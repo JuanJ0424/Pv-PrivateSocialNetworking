497a751e1dfb29cb5f19fddfca0d7d4b18976834
Said Pelaez <diego.210@hotmail.es>
2014-10-01 09:39:58 -0500

    More UI

--- app/assets/javascripts/personalJS/res.js
+++ app/assets/javascripts/personalJS/res.js
@@ -1,9 +1,12 @@
 $(window).resize(function(){
 		doRes();
+		
 	});
 function doRes(){
 	var a=1;
 	var s=$('.contentI').width();
+	var h=$(window).height();
+	var aux=200;
 	if(s<=imgGrind*6){
 		$('.spacer').html("");
 		a=1;
@@ -19,5 +22,26 @@ function doRes(){
 	}
 	if(s>=imgGrind*6){
 		$('.spacer').html("<br>");
+	}
+	if(s<=imgGrind*6 && h<833){
+			imgGrind=100;
+	
+			$('.basic').each(function(){
+				doGrow(0,$(this));
+			});
+		}else{
+			$('.basic').css("display","inline");
+		}
+	if(h<628){
+		imgGrind=150;
+	
+		$('.basic').each(function(){
+			doGrow(0,$(this));
+		});
+	}else{
+		imgGrind=aux;
+		$('.basic').each(function(){
+			doGrow(0,$(this));
+		});
 	}
 }
